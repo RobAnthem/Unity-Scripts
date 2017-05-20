3009980d7b1d0b241d95e4394d35793b38cf4bd0
Robin Trantham <robintrantham@gmail.com>
2017-05-19 22:12:36 -0600

    Added SceneSwitcher and Inspector
    SceneSwitcher allows you to easily add scene switching to any button, or a timed switched to any monobehaviour.

---
+++ SceneSwitcher.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+namespace SpaceRace
+{
+    public class SceneSwitcher : MonoBehaviour
+    {
+        public bool timedSwitch;
+        public float switchTime;
+        private float currentTime;
+        public int destination;
+        public enum ChangeType { None, Time, SceneId, Next, Previous, Restart }
+        public ChangeType switchType;
+        public bool AddClickDelegate;
+        void OnEnable()
+        {
+            if (switchType == ChangeType.Next)
+            {
+                gameObject.GetComponent<Button>().onClick.AddListener(Next);
+            }
+            else if (switchType == ChangeType.Previous)
+            {
+                gameObject.GetComponent<Button>().onClick.AddListener(Previous);
+            }
+            else if (switchType == ChangeType.Restart)
+            {
+                gameObject.GetComponent<Button>().onClick.AddListener(Restart);
+            }
+            else if (switchType == ChangeType.SceneId)
+            {
+                gameObject.GetComponent<Button>().onClick.AddListener(SwitchScene);
+            }
+            else if (switchType == ChangeType.Time)
+            {
+                currentTime = switchTime;
+                timedSwitch = true;
+            }
+        }
+        void OnDisable()
+        {
+            if (switchType == ChangeType.Next)
+            {
+                gameObject.GetComponent<Button>().onClick.RemoveListener(Next);
+            }
+            else if (switchType == ChangeType.Previous)
+            {
+                gameObject.GetComponent<Button>().onClick.RemoveListener(Previous);
+            }
+            else if (switchType == ChangeType.Restart)
+            {
+                gameObject.GetComponent<Button>().onClick.RemoveListener(Restart);
+            }
+            else if (switchType == ChangeType.SceneId)
+            {
+                gameObject.GetComponent<Button>().onClick.RemoveListener(SwitchScene);
+            }
+            else if (switchType == ChangeType.Time)
+            {
+                timedSwitch = false;
+            }
+        }
+        void Update()
+        {
+            if (timedSwitch)
+            {
+                UpdateTime(Time.deltaTime);
+            }
+        }
+        void UpdateTime(float time)
+        {
+            currentTime -= time;
+            if (currentTime < 0)
+            {
+                timedSwitch = false;
+                SwitchScene();
+            }
+        }
+        public void SwitchScene()
+        {
+            SceneManager.LoadSceneAsync(destination);
+        }
+        public void Restart()
+        {
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        }
+        public void Next()
+        {
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex +1);
+        }
+        public void Previous()
+        {
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+}
---
+++ SceneSwitcherInspector.cs
@@ -0,0 +1,39 @@
+﻿#if (UNITY_EDITOR) //Prevent this file from being compiled into your game.
+
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using SpaceRace;
+[CustomEditor(typeof(SceneSwitcher))] //Tell unity that this is an Editor for objects of type colorFader.
+public class SceneSwitcherInspector : Editor //We must derive from Editor to use the Editor GUI.
+{
+    public SceneSwitcher sceneSwitcher;
+    public override void OnInspectorGUI() //We are overriding the unity-generated inspector for our colorFader.
+    {
+        sceneSwitcher = target as SceneSwitcher; //Getting out object. Checking for null is not necessary, unity will not throw an error if it is destroyed.
+        EditorGUILayout.BeginVertical(EditorStyles.helpBox); //Begin our layout, with a nice helpbox look.
+        sceneSwitcher.switchType = (SceneSwitcher.ChangeType)EditorGUILayout.EnumPopup("Type", sceneSwitcher.switchType);
+        if (sceneSwitcher.switchType != SceneSwitcher.ChangeType.None)
+        {
+
+            if (sceneSwitcher.switchType == SceneSwitcher.ChangeType.Time)
+            {
+                sceneSwitcher.switchTime = EditorGUILayout.FloatField("Time in Seconds", sceneSwitcher.switchTime);
+                sceneSwitcher.destination = EditorGUILayout.IntField("Destination ID", sceneSwitcher.destination);
+            }
+            else if (sceneSwitcher.switchType == SceneSwitcher.ChangeType.SceneId || sceneSwitcher.switchType == SceneSwitcher.ChangeType.Time)
+            {
+                sceneSwitcher.destination = EditorGUILayout.IntField("Destination ID", sceneSwitcher.destination);
+            }
+        }
+        EditorGUILayout.EndVertical(); //End the GUI.
+        if (sceneSwitcher.switchType != SceneSwitcher.ChangeType.Time)
+        {
+            if (sceneSwitcher.gameObject.GetComponent<Button>() == null)
+            {
+                sceneSwitcher.switchType = SceneSwitcher.ChangeType.None;
+            }
+        }
+    }
+}
+#endif
