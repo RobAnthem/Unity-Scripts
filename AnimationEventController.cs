33a26857f170a3928f7caa81a8591dc769d99279
Robin Trantham <robintrantham@gmail.com>
2022-07-24 11:07:23 -0600

    Add files via upload

---
+++ AnimationEventController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventController : MonoBehaviour
+{
+	public UnityEngine.Events.UnityEvent[] events;
+	public void OnAnimationEvent(AnimationEvent e)
+	{
+		events[e.intParameter].Invoke();
+	}
+	public void ShakeScreen(float val)
+	{
+		CamFollow.Instance.Shake(val);
+	}
+}
